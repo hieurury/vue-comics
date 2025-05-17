62dc518c2fae374e3efda7f0210dc91670cc9a45
hieurury <hieurury007@gmail.com>
2025-05-17 14:56:23 +0700

    test darkmode

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from './App.vue';
 import router from './routers/index';
+import './css/tailwind.css'; // Import Tailwind CSS
 
 createApp(App).use(router).mount('#app');
